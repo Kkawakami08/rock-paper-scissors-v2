d31613b4039e6f032442e45806fb3cd1272d3d97
kkawakami08 <k.kawakami08@gmail.com>
2022-02-24 13:12:50 -0800

    add final winner after 5 points

--- javascript.js
+++ javascript.js
@@ -12,41 +12,17 @@ body.appendChild(container2);
 let endResult = document.createElement("p");
 endResult.id = "endResult";
 endResult.innerText = "Rock, paper, scissors... Shoot!";
-container2.appendChild(endResult);
+container.appendChild(endResult);
 
 let cScore = document.createElement("p");
 cScore.textContent = `Computer Score: ${computerScore}`;
 
 let pScore = document.createElement("p");
-pScore.textContent = `Player Score: ${playerScore}`;
+pScore.textContent = `Your Score: ${playerScore}`;
 
 container2.appendChild(cScore);
 container2.appendChild(pScore);
 
-// function game() {
-//   for (i = 1; i <= 5; i++) {
-//     let playerInput = prompt("Choose rock, paper, or scissors");
-//     let playerSelection = playerInput.toLowerCase();
-//     console.log(`Computer chose: ${computerSelection}`);
-//     console.log(`You chose: ${playerSelection}`);
-//     console.log(oneRound(playerSelection, computerSelection));
-//     console.log(`Your score: ${playerScore}`);
-//     console.log(`Computer's score: ${computerScore}`);
-//   }
-//   if (computerScore > playerScore) {
-//     console.log(
-//       `Final score: Computer = ${computerScore} Human = ${playerScore}`
-//     );
-//     console.log("You lose!!!!");
-//   } else if (computerScore < playerScore) {
-//     console.log(
-//       `Final score: Computer = ${computerScore} Human = ${playerScore}`
-//     );
-//     console.log("You win!!!!!");
-//   }
-// }
-
-// console.log(game());
 let rockButton = document.getElementById("rockbtn");
 rockButton.value = "rock";
 
@@ -67,11 +43,7 @@ function oneRound(e) {
       compArray[Math.floor(Math.random() * compArray.length)];
     return randomSelection;
   }
-
   let computerSelection = computerPlay();
-
-  console.log(`You chose: ${e.target.value}`);
-  console.log(`Computer chose: ${computerSelection}`);
 
   let result = "";
 
@@ -79,40 +51,42 @@ function oneRound(e) {
     if (computerSelection === "rock") {
       result = "It's a tie!";
     } else if (computerSelection === "paper") {
-      result = "You lose!";
+      result = "You lose! Computer chose paper.";
       computerScore++;
     } else if (computerSelection === "scissors") {
-      result = "You win!";
+      result = "You win! Computer chose scissors.";
       playerScore++;
     }
   } else if (e.target.value === "paper") {
     if (computerSelection === "rock") {
-      result = "You win!";
+      result = "You win! Computer chose rock.";
       playerScore++;
     } else if (computerSelection === "paper") {
       result = "It's a tie!";
     } else if (computerSelection === "scissors") {
-      result = "You lose!";
+      result = "You lose! Computer chose scissors.";
       computerScore++;
     }
   } else if (e.target.value === "scissors") {
     if (computerSelection === "rock") {
-      result = "You lose!";
+      result = "You lose! Computer chose rock.";
       computerScore++;
     } else if (computerSelection === "paper") {
-      result = "You win!";
+      result = "You win! Computer chose paper.";
       playerScore++;
     } else if (computerSelection === "scissors") {
       result = "It's a tie!";
     }
-  } else {
-    console.log("baa");
   }
   endResult.innerText = result;
   cScore.innerText = `Computer Score: ${computerScore}`;
-  pScore.innerText = `Player Score: ${playerScore}`;
+  pScore.innerText = `Your Score: ${playerScore}`;
 
-  console.log("");
+  if (computerScore == 5) {
+    endResult.innerText = "Computer Wins!";
+  } else if (playerScore == 5) {
+    endResult.innerText = "You win!";
+  }
 }
 
 container.appendChild(rockButton);
